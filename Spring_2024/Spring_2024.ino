cb55419863fbf7d2f1c9993bfcde5514908adecb
Everett Kernen <everett.kernen@idp.wichita.edu>
2024-04-10 15:30:02 -0500

    fully rewrote arduino file for pcb and using servo controls for both motors and steering

--- Spring_2024/Spring_2024.ino
+++ Spring_2024/Spring_2024.ino
@@ -6,124 +6,53 @@
 //but if you do still have questions, email me at ekernen2@gmail.com with the subject line reading "GoBabyGo" along with whatever else
 //I'll try to respond in a semi-timely manner
 
+//It should be noted this code uses a custom PCB to connect wires together. See details for that in the README, if I remember
+
 #include <Servo.h>
 
-
-//PINS USED IN ARDUINO
-//either plug wires into the pins we've already written or write in the pins you're using
-//a number is equal to its digital counterpart, i.e. digital pin 10 --> 10
-//for analog, use prefix A, i.e. analog pin 5 --> A5
-const int SPEEDCONTROLLERPOSITIVE = 12;
-const int SPEEDCONTROLLERPINin = A0; //TBD
-const int SPEEDCONTROLLERNEGATIVE = 11;
-const int RCTRANSMITTERONEin = 10; //PWM channel
-const int RCTRANSMITTERTWOin = 6; //PWM channel
-const int RCTRANSMITTERTHREEin = 5; //PWM channel
-const int LINEARACTUATORSIGNALout = 3; //PWM channel
-const int JOYSTICKHORIZONTAL = A0;
-const int JOYSTICKVERTICAL = A2;
+//TODO:
+//test (obviously)
+//get correct constants
+//take correct rc inputs
+//modify those inputs to microsecond outputs
+//set up setJoystick func
+//set up emergency stop func
 
 
-
-
-const int JFWDBWDPIN = A2;  //pin number for forward and backwards input on the joystick
-const int JSIDEWAYSPIN = A4;//pin number for left and right input on the joystick
-const int GAS = 2;         //pin number for the Gas relay input
-const int FWD = 4;          //forward relay
-const int BWD = 6;          //backward relay
-//IF YOU DO NOT IMPLEMENT THESE CONTROLLER INPUTS, just connect these pins to ground
-const int RIGHTMOTOR = 10;   //signal in from the remote that you are turning right
-const int LEFTMOTOR = 12;    //signal in from the remote that you are turning left
-const int ENCODER = A6;     //encoder signal pin (the encoder is what measures where the steering shaft is rotationally)
-const int STEERMOTOR = 8;   //motor controller output pin
+//PINS
+const int JOYSTICKSWITCH = 12;
+const int RC_SPEED = 10; //PWM
+const int RC_STEERING = 9; //PWM
+const int RC_EMERGENCYSTOP = 6; //PWM
+const int STEERING_output = 5; //PWM - this controls either the linear actuator or servo, depending on which is selected to steer the car
+const int MOTOR_output = 3; //PWM
+const int SPEEDCONTROLLER = A0;
+const int JOYHORI = A2;
+const int JOYVERT = A4;
 
 //CONSTANT VALUES
-//these values should already work; however you may need to change them
-//if joystick is initializing too quickly or too late
-//if steering motor activates too often
-//if the steering isn't centered after "returning to center"
+const int JOY_MOTOR_MIDDLE = 0;
+const int JOY_STEER_MIDDLE = 0;
+const int JOY_DEADZONE = 250;         //how far away from center do you need to push the joystick before it's initialized?
 
-/*if you do need to change these values, I recommend you use the Serial Monitor to check the values.
-  As you turn the joystick, what values are you seeing and what values do you want to be seeing?
-  you can see the Serial Monitor by going to Tools->Serial Monitor */
-
-//CONSTANT VALUES TO CHECK AGAINST JOYSTICK VALUES
-//joystick initialization values
-//change these if joystick initializes too quickly or too late
-//middle is ~520 or so
-
-/*as mentioned below with the encoder values, we see JOYSTICKMIDDLE differently when the arduino is not connected to the computer
-  we started with 520, we moved to 630 (+110 again)
-  see lines 67-70 for more info*/
-const int JOYSTICKUPDOWNMIDDLE = 600;       //middle value for moving joystic up and down
-const int JOYSTICKLEFTRIGHTMIDDLE = 730;    //not entirely sure why the values are different for fwd/bwd and left/right, probably improper manufacturing
-const int JOYSTICKDEADZONE = 250;           //how far away from center do you need to push the joystick before it's initialized?
-const int FWDVALUE = JOYSTICKUPDOWNMIDDLE - JOYSTICKDEADZONE;   //used to check "if joystick forward value is above FWDVALUE, go forward"
-const int BWDVALUE = JOYSTICKUPDOWNMIDDLE + JOYSTICKDEADZONE;   //same as above, just if below BWDVALUE, go backwards
-const int LEFTVALUE = JOYSTICKLEFTRIGHTMIDDLE - JOYSTICKDEADZONE;  //same as above, just left
-const int RIGHTVALUE = JOYSTICKLEFTRIGHTMIDDLE + JOYSTICKDEADZONE; //same as above, just right
-//IF JOYSTICK CAUSES CAR TO DO THE OPPOSITE THING ITS SUPPOSED TO
-//you probably oriented the joystick differently than how we did, try modifying lines 45-51 by switching + with -
-//and modify lines 165, 171, 210, and 220
-//line numbers may not be entirely accurate, they refer to the if statements
-//e.g. switch "if (JFWDBWD > FWDVALUE)" to "if (JFWDBWD < FWDVALUE)"
-
-//CONSTANT VALUES TO CHECK AGAINST ENCODER VALUE
-/*steering centering values
-  change these if steering doesn't return to center properly
-  values go from 0-1023, clockwise turning moves the number up
-  you WILL need to modify ENCODERCENTER to begin with, it depends on how you install the encoder
-  make sure you install the encoder such that when you turn, it doesn't loop around from, for example, 800 to 200 by going 800->1023 then 0->200
-  that will view to you as the edges are 800 and 200, realign the encoder such that the middle and both edges are all within one loop of 0-1023*/
-
-/*when you unplug the usb, the steering goes off center
-  we think this is just due to processing differences between computer and arduino
-  the processing differences are irrelevant to you as the user, all you need to know is
-  you'll have to play with ENCODERCENTER to fix this issue. We went from 590 to 700*/
-const int ENCODERCENTER = 680;
-//modify these two if the center range is too broad or too narrow for your taste
-/*the logic used during use is "if the steering is outside of the ENCODERCOARSE range (and joystick isn't pushed left or right),
-  go full speed towards center. Once you're inside of the ENCODERCOARSE range, go half speed until in ENCODERFINE range. Once inside the fine range,
-  you're close enough to center" */
-//The steering rack is fairly loose so a tighter ENCODERFINE didn't do anything for us; play with it if you'd like though
-const int ENCODERFINE = 30;
-const int ENCODERCOARSE = 70;
-//Encoder right and left are the limits of where the wheels are turned all the way one direction
-//once it reaches this end, we send a message to the steer motor to not turn anymore
-const int ENCODERRIGHT = ENCODERCENTER - 50;
-const int ENCODERLEFT = ENCODERCENTER + 50;
-//IF THE ENCODER IS INSTALLED BACKWARDS OR THE STEERING RETURNS TO CENTER THE WRONG WAY
-//play with lines 81, 82; 232, 234, 243, and 245 by switching + with - and < with >
-//same as line 56, line numbers may not be entirely accurate. They refer to the if statements
+const int motor_bwdSpeed = 500;
+const int motor_zeroSpeed = 1500;
+const int motor_fwdSpeed = 2500;
+const int steering_left = 500;
+const int steering_middle = 1500;
+const int steering_right = 2500;
 
 
-//SPEED VALUES USED FOR STEERING
-/*how fast the steering turns
-  for our motor controller, we set the arduino to view it as a servo
-  as such, we use writeMicroseconds to control the speed
-  this is only relevant if you don't use the same motor controller or you set it up differently
-  if you don't or you just want more information, visit https://www.arduino.cc/en/Reference/ServoWriteMicroseconds or the arduino main reference page
-  anyways, for our motor controller, full speed one way is 2500; full speed the other way is 500
-  1500 is middle (no turning at all)*/
 
-//if the joystick turns the wrong way, switch the right and left values
-const int FULLSPEEDRIGHT = 750;
-const int PARTSPEEDRIGHT = 1150;
-const int ZEROSPEED = 1500;
-const int PARTSPEEDLEFT = 1850;
-const int FULLSPEEDLEFT = 2250;
-
-//-----DO NOT MODIFY PAST THIS LINE UNLESS YOU KNOW WHAT YOU'RE DOING-----
-
-
-//INTEGERS USED TO READ VALUES FROM JOYSTICK
-int JFWDBWD;
-int JLEFTRIGHT;
-
-//INTEGER TO READ VALUE FROM ENCODER (piece attached to steering shaft)
-int ENCODERVAL;
+//VARIABLES
+int joy_motorvalue;
+int joy_steervalue;
+int rc_motor;
+int rc_steer;
+int rc_stop;
 
 //Motor Controller (viewed by arduino as a servo)
+Servo MOTOR
 Servo STEER;
 
 //-------------------FUNCTIONS START HERE--------------------------------
@@ -134,139 +63,103 @@ void setup() {
   Serial.begin(9600);   //tells arduino how fast to interact with the Serial Monitor
 
   //set input and output pins for arduino
-  pinMode(JFWDBWDPIN, INPUT);
-  pinMode(JSIDEWAYSPIN, INPUT);
-  pinMode(ENCODER, INPUT);
-  pinMode(FWD, OUTPUT);
-  pinMode(BWD, OUTPUT);
-  pinMode(GAS, OUTPUT);
-  pinMode(RIGHTMOTOR, INPUT);
-  pinMode(LEFTMOTOR, INPUT);
-  STEER.attach(STEERMOTOR, 500, 2500);
+  pinMode(JOYSTICKSWITCH, INPUT);
+  pinMode(RC_SPEED, INPUT);
+  pinMode(RC_STEERING, INPUT);
+  pinMode(RC_EMERGENCYSTOP, INPUT);
+  pinMode(SPEEDCONTROLLER, INPUT);
+  pinMode(JOYHORI, INPUT);
+  pinMode(JOYVERT, INPUT);
+  
+  MOTOR.attach(MOTOR_output)
+  STEER.attach(STEERING_output);
 
-  //safekeeping measure so relays are set to the car not running to the races on startup
-  //also, the steering motor is set to not do anything
-  digitalWrite(GAS, LOW);
-  digitalWrite(FWD, LOW);
-  digitalWrite(BWD, LOW);
-  STEER.writeMicroseconds(ZEROSPEED);
+  MOTOR.writeMicroseconds(motor_zerospeed);
+  STEER.writeMicroseconds(steering_middle);
+
+  setJoystick();
 }
 
 // put your main code here, to run repeatedly:
 void loop() {
 
-  //take in analog input to read what values the joystick is sending
-  JFWDBWD = analogRead(JFWDBWDPIN);
-  JLEFTRIGHT = analogRead(JSIDEWAYSPIN);
-  ENCODERVAL = analogRead(ENCODER);
+  //take in analog input to read what values the joystick and RC controller are sending
+  joy_motorvalue = analogRead(JOYVERT);
+  joy_steervalue = analogRead(JOYHORI);
+  rc_motor = GetPWM(RC_MOTOR);
+  rc_steer = GetPWM(RC_STEER);
+  rc_stop = GetPWM(RC_EMERGENCYSTOP);
+
 
   //print out values to the serial monitor so that we can look at them
-  Serial.print("Yval: ");
-  Serial.println(JFWDBWD);
-  Serial.print("Xval: ");
-  Serial.println(JLEFTRIGHT);
-  Serial.print("Steering: ");
-  Serial.println(ENCODERVAL);
+  Serial.print("Joy motor:          ");
+  Serial.println(joy_motorvalue);
+  Serial.print("Joy steer:          ");
+  Serial.println(joy_steervalue);
+  Serial.print("RC motor:           ");
+  Serial.println(rc_motor);
+  Serial.print("RC steer:           ");
+  Serial.println(rc_steer);
+  Serial.print("RC Emergency Stop:  ");
+  Serial.println(rc_stop);
   Serial.println();
-  Serial.print("Left Value:        ");
-  Serial.println(digitalRead(LEFTMOTOR));
-  Serial.print("Right Value:                          ");
-  Serial.println(digitalRead(RIGHTMOTOR));
 
-
-  if (JFWDBWD < FWDVALUE) {
-    //if joystick is forward, go forward
-    digitalWrite(FWD, HIGH);
-    digitalWrite(GAS, HIGH);
-    digitalWrite(BWD, LOW);
+  // TEST FOR EMERGENCY STOP
+  if (rc_stop != 0) {
+    EmergencyStop();
   }
-  else if (JFWDBWD > BWDVALUE) {
-    //if joystick is backward, go backwarde
-    digitalWrite(BWD, HIGH);
-    digitalWrite(GAS, HIGH);
-    digitalWrite(FWD, LOW);
+
+  // SEND SIGNAL TO MOTOR
+  if (rc_motor != 0) { // prioritize the rc controller
+    MOTOR.writeMicroseconds(rc_motor);
   }
   else {
-    //"if joystick y value is centered"
-    //set relays so nothing is on
-    digitalWrite(FWD, LOW);
-    digitalWrite(BWD, LOW);
-    digitalWrite(GAS, LOW);
-  }
-
-  /*we test for digitalRead before we test for joystick values to prioritize the parent remote
-    this does add complexity to the build process, as it requires you add the ability to read the values
-    the computer is trying to send to the motor. We did this via two voltage regulators (12V down to 5V) and two diodes*/
-  if (digitalRead(RIGHTMOTOR) == HIGH) {
-    //turn right
-    if (ENCODERVAL < ENCODERRIGHT) {
-      //if it's already all the way right, no need to turn further and strain the motor/gears
-      STEER.writeMicroseconds(ZEROSPEED);
+    if (joy_motorvalue > JOY_MOTORMIDDLE + JOY_DEADZONE) {
+      MOTOR.writeMicroseconds(motor_fwdSpeed);
+    }
+    else if (joy_motorvalue < JOY_MOTORMIDDLE - JOY_DEADZONE) {
+      MOTOR.writeMicroseconds(motor_bwdSpeed);
     }
     else {
-      STEER.writeMicroseconds(FULLSPEEDRIGHT);
+      MOTOR.writeMicroseconds(motor_zeroSpeed);
     }
   }
-  else if (digitalRead(LEFTMOTOR) == HIGH) {
-    //turn left
-    if (ENCODERVAL > ENCODERLEFT) {
-      //if it's already all the way left, no need to turn further and strain the motor/gears
-      STEER.writeMicroseconds(ZEROSPEED);
-    }
-    else {
-      STEER.writeMicroseconds(FULLSPEEDLEFT);
-    }
-  }
-
-
-  else if (JLEFTRIGHT > RIGHTVALUE) {
-    //turn right
-    if (ENCODERVAL < ENCODERRIGHT) {
-      //if it's already all the way right, no need to turn further and strain the motor/gears
-      STEER.writeMicroseconds(ZEROSPEED);
-    }
-    else {
-      STEER.writeMicroseconds(FULLSPEEDRIGHT);
-    }
-  }
-  else if (JLEFTRIGHT < LEFTVALUE) {
-    //turn left
-    if (ENCODERVAL > ENCODERLEFT) {
-      //if it's already all the way left, no need to turn further and strain the motor/gears
-      STEER.writeMicroseconds(ZEROSPEED);
-    }
-    else {
-      STEER.writeMicroseconds(FULLSPEEDLEFT);
-    }
+  
+  // SEND SIGNAL TO STEERING
+  if (rc_steer != 0) { // prioritize the rc controller
+    STEER.writeMicroseconds(rc_steer);
   }
   else {
-    //"if joystick x value is centered"
-    if (ENCODERVAL > (ENCODERCENTER + ENCODERFINE)) {
-      //if the steering is turned to the left
-      if (ENCODERVAL > (ENCODERCENTER + ENCODERCOARSE)) {
-        //if it's turned all the way to the left, turn right full speed
-        STEER.writeMicroseconds(PARTSPEEDRIGHT);
-      }
-      else {
-        //if it's turned only a little to the left, turn right at a lower speed
-        STEER.writeMicroseconds(PARTSPEEDRIGHT);
-      }
+    if (joy_motorvalue > JOY_MOTORMIDDLE + JOY_DEADZONE) {
+      STEER.writeMicroseconds(steering_right);
     }
-    else if (ENCODERVAL < (ENCODERCENTER - ENCODERFINE)) {
-      //if the steering is turned to the right
-      if (ENCODERVAL < (ENCODERCENTER - ENCODERCOARSE)) {
-        //if it's turned all the way to the right, turn left full speed
-        STEER.writeMicroseconds(PARTSPEEDLEFT);
-      }
-      else {
-        //if it's turned only a little to the right, turn left at a lower speed
-        STEER.writeMicroseconds(PARTSPEEDLEFT);
-      }
+    else if (joy_motorvalue < JOY_MOTORMIDDLE - JOY_DEADZONE) {
+      STEER.writeMicroseconds(steering_left);
     }
     else {
-      //if the joystick is centered and so is the steering, do nothing
-      STEER.writeMicroseconds(ZEROSPEED);
+      STEER.writeMicroseconds(steering_middle);
     }
   }
-
 }
+
+  void setJoystick(){
+    //is joystickswitch flipped?
+  }
+
+  byte GetPWM(byte pin)
+  {
+    unsigned long highTime = pulseIn(pin, HIGH, 50000UL);  // 50 millisecond timeout
+    unsigned long lowTime = pulseIn(pin, LOW, 50000UL);  // 50 millisecond timeout
+
+    // pulseIn() returns zero on timeout
+    if (highTime == 0 || lowTime == 0)
+      return digitalRead(pin) ? 100 : 0;  // HIGH == 100%,  LOW = 0%
+
+    return (100 * highTime) / (highTime + lowTime);  // highTime as percentage of total cycle time
+  }
+
+  void EmergencyStop() {
+    // does rc send stop consistently? or just once?
+    // if consistently, wait for release of button
+    while (rc_stop);
+  }
